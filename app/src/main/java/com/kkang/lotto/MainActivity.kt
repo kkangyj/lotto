90c69b06c4709f0f03677cb7bca920c1e7391256
강윤정 <kkang_yj36@naver.com>
2020-10-09 14:28:01 +0900

    랜덤으로 번호 추출 기능 추가

--- app/src/main/java/com/kkang/lotto/MainActivity.kt
+++ app/src/main/java/com/kkang/lotto/MainActivity.kt
@@ -4,14 +4,53 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    fun getRandomLottoNumber(): Int {
+        return Random.nextInt(45) + 1
+    }
+
+    //셔플이용
+    fun getShuffleLottoNumber(): MutableList<Int> {
+        val list = mutableListOf<Int>()
+        for (number in 1..45) {
+            list.add(number)
+        }
+
+        list.shuffle()
+
+        return list.subList(0, 6)
+    }
+
+    //랜덤함수 이용
+    fun getRandomLottoNumbers(): MutableList<Int> {
+        val lottoNumbers = mutableListOf<Int>()
+
+        for (i in 1..6) {
+            var number = 0
+
+            do {
+                number = getRandomLottoNumber()
+            } while (lottoNumbers.contains(number))
+            lottoNumbers.add(number)
+        }
+
+        return lottoNumbers
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        randomCard.setOnClickListener{
-            startActivity(Intent(this, ResultActivity::class.java))
+        randomCard.setOnClickListener {
+
+            val intent = Intent(this, ResultActivity::class.java)
+
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+
+            startActivity(intent)
         }
 
         constellationCard.setOnClickListener {
@@ -21,6 +60,7 @@ class MainActivity : AppCompatActivity() {
         nameCard.setOnClickListener {
             startActivity(Intent(this, NameActivity::class.java))
         }
+
 
     }
 }
