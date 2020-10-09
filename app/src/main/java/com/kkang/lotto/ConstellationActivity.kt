361a86934f9f478afe67627fed3e59a3584a1063
강윤정 <kkang_yj36@naver.com>
2020-10-09 12:55:03 +0900

    버튼 누르면 화면 전환은 됨

--- app/src/main/java/com/kkang/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/kkang/lotto/ConstellationActivity.kt
@@ -1,11 +1,18 @@
 package com.kkang.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_constellation.*
 
 class ConstellationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
+
+        goResultBtn.setOnClickListener {
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+
     }
 }
--- app/src/main/java/com/kkang/lotto/MainActivity.kt
+++ app/src/main/java/com/kkang/lotto/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.kkang.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        randomCard.setOnClickListener{
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+
+        constellationCard.setOnClickListener {
+            startActivity(Intent(this, ConstellationActivity::class.java))
+        }
+
+        nameCard.setOnClickListener {
+            startActivity(Intent(this, NameActivity::class.java))
+        }
+
     }
 }
